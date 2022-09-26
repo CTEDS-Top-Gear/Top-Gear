7254a1fa9e8f38d6387ed8b25123ea1c3c0ff498
Novaes98 <caiquenovaes098@gmail.com>
2022-09-26 19:17:31 -0300

    Atualização da pista!

--- Fila_Pista.cs
+++ Fila_Pista.cs
@@ -5,43 +5,71 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("                               %  #%%%%%%%%%%%/ .%                              \r\n                              &&%%%%%%%%%%%%%%%%%%(                             \r\n                              &%%%%%%%%%%%%%%%%%%%(                             \r\n                              &%%%%%%%%%%%%%%%%%%%(                             \r\n                           /&&%%%%.           ,%%%(&%.                          \r\n                              &(%              *%%(                             \r\n                              %/ %(           %/ %(                             \r\n                              %/  ,%%%%%%%%%%%   %(                             \r\n                              %/   %%%%%%%%%%#   %(                             \r\n                              %/   %%%%%%%%%%#   %(                             \r\n                              %/ .%  .,//*,  #%  %(                             \r\n                              %%%              *%%(                             \r\n                              &%%%%%%%%%%%%%%%%%%%(                             \r\n                              &%%%%%%%%%%%%%%%%%%%(                             \r\n                               %%&&%&%%%%%%%%%&%&#");
-            race();
-            Console.WriteLine("|          |");
-            Console.WriteLine("|   |--|   |");
-            Console.WriteLine("|          |");
-        }
 
         public static void race()
         {
-            ArrayList list = new ArrayList();
+            Random rnd = new Random();
+
+            List<int> list = new List<int>();
+            int posicaoPista = (Console.WindowWidth - 20) / 2;
             int timer = 1000;
+
+            initial(list, posicaoPista);
+
+            string pista = "|                    |";
 
             while (true)
             {
+                System.Threading.Thread.Sleep(timer);
 
-                if (list.Count < 50)
+                Console.Clear();
+
+                for (int i = 0; i < list.Count; i++)
                 {
-                    line(list);
+                    Console.SetCursorPosition(list[i], Console.CursorTop);
+                    Console.WriteLine(pista);
                 }
 
-                System.Threading.Thread.Sleep(timer);
+                ImprimePista(list, rnd.Next(3));
 
             }
 
-           
-        }  
 
-        public static void line(ArrayList a)
+        }
+
+        public static void initial(List<int> lista, int racePosition)
         {
             for (int i = 0; i < 10; i++)
             {
-                a.Add("|           |");
+                lista.Add(racePosition);
             }
         }
 
+        public static void ImprimePista(List<int> lista, int random)
+        {
+            int posicao = lista[0];
+
+            if (random == 0)
+            {
+                lista.RemoveAt(9);
+                lista.Insert(0, posicao);
+                return;
+            }
+            else if (random == 1)
+            {
+                lista.RemoveAt(9);
+                lista.Insert(0, posicao + 1);
+                return;
+            }
+            else
+            {
+                lista.RemoveAt(9);
+                lista.Insert(0, posicao - 1);
+                return;
+            }
+
+
+        }
 
     }
 }
